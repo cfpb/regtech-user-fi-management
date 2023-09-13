2f8f8bdf9f9e5bd37a1b76131dc41dd920b125aa
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2023-09-13 14:55:00 -0400

    feat: open endpoints env vars optimization

--- src/dependencies.py
+++ src/dependencies.py
@@ -9,6 +9,13 @@ from entities.engine import get_session
 from entities.repos import institutions_repo as repo
 
 
+OPEN_ENDPOINTS = os.getenv(
+    "OPEN_ENDPOINT_PATHS",
+    "/v1/admin/me,/v1/institutions,/v1/institutions/domains/allowed",
+).split(",")
+OPEN_METHODS = os.getenv("OPEN_ENDPOINT_METHODS", "GET").split(",")
+
+
 async def check_domain(
     request: Request, session: Annotated[AsyncSession, Depends(get_session)]
 ) -> None:
@@ -22,16 +29,9 @@ async def check_domain(
 
 
 def request_needs_domain_check(request: Request) -> bool:
-    open_endpoints = os.getenv(
-        "OPEN_ENDPOINT_PATHS",
-        "/v1/admin/me,/v1/institutions,/v1/institutions/domains/allowed",
-    )
-    open_methods = os.getenv("OPEN_ENDPOINT_METHODS", "GET")
-    endpoints = open_endpoints.split(",")
-    methods = open_methods.split(",")
     return not (
-        request.scope["path"].rstrip("/") in endpoints
-        and request.scope["method"] in methods
+        request.scope["path"].rstrip("/") in OPEN_ENDPOINTS
+        and request.scope["method"] in OPEN_METHODS
     )
 
 
