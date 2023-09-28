ef35ae9b60122e5cf42be487e3465d06db5fede9
Aldrian Harjati <aldrian.harjati@cfpb.gov>
2023-09-28 12:36:44 -0400

    add src to syspath

--- db_revisions/env.py
+++ db_revisions/env.py
@@ -7,7 +7,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from src.entities import models
+from entities import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
